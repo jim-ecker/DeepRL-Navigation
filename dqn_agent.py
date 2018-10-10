535e72038e07bc0c024651125d47e3ffb054cba7
Jim Ecker <jim.ecker@gmail.com>
2018-10-10 17:29:31 -0400

    Changed default PyTorch device to cuda:1 to use spare GPU on my system

--- dqn_agent.py
+++ dqn_agent.py
@@ -15,7 +15,7 @@ TAU = 1e-3  # for soft update of target parameters
 LR = 5e-4  # learning rate
 UPDATE_EVERY = 4  # how often to update the network
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 
 
 class Agent():
